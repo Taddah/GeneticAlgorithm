aead94503e818958bc9da65f4e51debdd9e44e82
Tada <thomas.bernard72@gmail.com>
2018-12-03 10:56:27 +0100

    Doc #1

--- GeneticAlgorithm/src/main/Main.java
+++ GeneticAlgorithm/src/main/Main.java
@@ -3,6 +3,13 @@ package main;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Main--- Exécution de la librairie
+ * 
+ * @author Thomas
+ * @since 03/12/2018
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
--- GeneticAlgorithm/src/model/Individu.java
+++ GeneticAlgorithm/src/model/Individu.java
@@ -1,5 +1,12 @@
 package model;
 
+/**
+ * Individu--- Un individu représente une solution possible au problème
+ * 
+ * @author Thomas
+ * @since 03.12.2018
+ *
+ */
 public class Individu {
 
 }
